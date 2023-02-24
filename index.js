d2523492e8cf817d2679429e9f2b73951fdb781b
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-02-24 07:11:46 +0300

    Completed Assignment.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(val="roller-skate") {
+	return `This Saturday, I want to ${val}!`;
+} 
+
+function mondayWork(val = 'go to the office') {
+	return `This Monday, I will ${val}.`;
+}
+
+function wrapAdjective(adjective) {
+	return function (statement) {
+		return `You are ${adjective}${statement}${adjective}!`
+	}
+}
